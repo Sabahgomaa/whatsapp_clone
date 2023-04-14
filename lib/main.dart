db3aea1eabd335cd11d8921b8a7dba4e409144a3
Sabahgomaa <sabahgomaa576@gmail.com>
2023-04-14 07:15:53 +0200

    whatsapp

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'common/utils/colors.dart';
+import 'features/landing_screen/landing_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Whatsapp UI',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: backgroundColor,
+        appBarTheme: const AppBarTheme(
+          color: appBarColor,
+        ),
+      ),
+      home: const LandingScreen(),
+    );
+  }
+}
